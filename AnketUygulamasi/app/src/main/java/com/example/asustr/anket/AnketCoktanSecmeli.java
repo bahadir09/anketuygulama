278345e1c18d85184bd3d87a389fc602d2714316
bahadir09 <bahadirtelef9@gmail.com>
2018-04-22 01:38:48 +0300

    anket coktan secmeli secenek kaydet

--- AnketUygulamasi/app/src/main/java/com/example/asustr/anket/AnketCoktanSecmeli.java
+++ AnketUygulamasi/app/src/main/java/com/example/asustr/anket/AnketCoktanSecmeli.java
@@ -18,6 +18,7 @@ import org.ksoap2.transport.HttpTransportSE;
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -31,6 +32,7 @@ public class AnketCoktanSecmeli extends AppCompatActivity {
     Boolean sonuc;
     int soruTipi = 1;
     public static int soruID;
+    public String editTextSecenekler="";
 
 
     // MultiEditText referansı
@@ -51,20 +53,93 @@ public class AnketCoktanSecmeli extends AppCompatActivity {
         btnkaydet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 /*
                 veriler = new ArrayList<String>();
                 // Verilerimizi nesnemizden alıyoruz.
                 veriler = multiEditText.getStringData();
                 // Listemizi tarayıp bir Log basalım.
                 for(int i=0; i<veriler.size(); i++){
-                    Log.d("AnketCoktanSecmeli", (i+1) + ".Seçenek: " + veriler.get(i));
+                    //Log.d("AnketCoktanSecmeli", (i+1) + ".Seçenek: " + veriler.get(i));
+                    String editTextSecenekler = veriler.get(i) + "@";
                 }
                 */
+
                 new WebServisAnketCoktanSecmeliSoruKaydet().execute();
 
             }
         });
+    }
+
+    private void Parcala () {
+        veriler = new ArrayList<String>();
+        // Verilerimizi nesnemizden alıyoruz.
+        veriler = multiEditText.getStringData();
+        // Listemizi tarayıp bir Log basalım.
+        for (int i = 0; i < veriler.size(); i++) {
+            //Log.d("AnketCoktanSecmeli", (i+1) + ".Seçenek: " + veriler.get(i));
+            editTextSecenekler += veriler.get(i) + "@";
+        }
+    }
+
+    class WebServisAnketCoktanSecmeliSecenekKaydet extends AsyncTask<Void, Void, Void> {
+
+
+        private static final String NAMESPACE = "http://service.dahafazlaoku.com/WebService1.asmx";
+        private static final String URL = "http://service.dahafazlaoku.com/WebService1.asmx?op=anketCoktanSecmeliSecenekKaydet";
+        private static final String SOAP_ACTION = "http://service.dahafazlaoku.com/WebService1.asmx/anketCoktanSecmeliSecenekKaydet";
+        private static final String METHOD_NAME = "anketCoktanSecmeliSecenekKaydet";
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+   /*Servise gitmeden önce çalışacak kod bloğu.*/
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+   /*Servise gidip geldiği zamanlarda çalışacak kod bloğu.*/
+
+            SoapObject spObjectGonder = new SoapObject(NAMESPACE, METHOD_NAME);
+            spObjectGonder.addProperty("secenekler", editTextSecenekler);
+            spObjectGonder.addProperty("soruId", soruID);
+   /*Tırnak içerisinde yeşil ile yazılmış olan KullanıcıAdı, Sifre web servisteki ilgili  metot ile aynı olmak zorundadır.*/
+            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            envelope.dotNet = true;/*envelope.dotNet = true; alanı web servisimiz Microsoft tabanlı olduğunu göstermektedir.*/
+            envelope.setOutputSoapObject(spObjectGonder);
+            try {
+                HttpTransportSE transport2 = new HttpTransportSE(URL);
+                transport2.debug = true;
+                transport2.call(SOAP_ACTION, envelope);
+      /*spObjectGonder Soap paketini HttpTransportSE üzerinden url oluşturarak gönderdik.*/
+                SoapPrimitive spPrimitiveGelen = (SoapPrimitive) envelope.getResponse();
+      /*Gelen soap paketini alıyoruz. “(SoapPrimitive)” demek tek bir parametre geri dönüş aldığımız için kullandık. Bize servisimiz 0-1 değerlerinden birisi döndürmektedir.*/
+                sonuc = Boolean.parseBoolean(spPrimitiveGelen.toString());
+      /*Integer Sonuc; oncreate üzerinde global olarak tanımlanmalıdır.*/
+      /*Gelen değer integere parse ederek aldık. Sonuc değişkenine atadık.*/
+            } catch (XmlPullParserException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+   /*Tüm işlemler bittikten sonra çalışacak kod bloğu.*/
+
+   /*ProgressDialog işlemini sonlandırıyoruz.*/
+            if (!sonuc) {
+
+                Toast.makeText(getApplicationContext(), "Soru Kaydedilemedi.", Toast.LENGTH_LONG).show();
+      /*Eğer servisten sıfır değeri gelmişse Kullanıcı Adı Şifre yanlış diye Toast mesajı veriyoruz.*/
+            } else {
+    /*Eğer servisten bir değeri gelmişse anasayfaya yönlendirme işlemi yapıyoruz.*/
+                new WebServisCoktanSecmeliSoruID().execute();
+                startActivity(new Intent(AnketCoktanSecmeli.this, AnketOlustur.class));
+                editTextSoru.setText("");
+            }
+        }
     }
 
     class WebServisAnketCoktanSecmeliSoruKaydet extends AsyncTask<Void, Void, Void> {
@@ -123,8 +198,10 @@ public class AnketCoktanSecmeli extends AppCompatActivity {
             } else {
     /*Eğer servisten bir değeri gelmişse anasayfaya yönlendirme işlemi yapıyoruz.*/
                 new WebServisCoktanSecmeliSoruID().execute();
-                startActivity(new Intent(AnketCoktanSecmeli.this, AnketOlustur.class));
-                editTextSoru.setText("");
+                Parcala();
+                new WebServisAnketCoktanSecmeliSecenekKaydet().execute();
+                //startActivity(new Intent(AnketCoktanSecmeli.this, AnketOlustur.class));
+                //editTextSoru.setText("");
             }
         }
     }
@@ -181,5 +258,4 @@ public class AnketCoktanSecmeli extends AppCompatActivity {
             else {}
         }
     }
-
 }
